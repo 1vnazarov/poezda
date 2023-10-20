93655be9bf83e5811885819bab85b1429774c81e
1vnazarov <nazaroffvana.boeing@yandex.ru>
2023-10-20 21:11:51 +0300

    Правочки

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 class TrainRoute(private val startCity: String, private val endCity: String) {
-    fun getRoute(): String {
-        return "$startCity - $endCity"
-    }
+    fun getRoute() = "$startCity - $endCity"
 }
 
 class TrainCar(val capacity: Int)
@@ -9,23 +7,17 @@ class TrainCar(val capacity: Int)
 class Train(private var route: TrainRoute) {
     val cars: MutableList<TrainCar> = mutableListOf()
 
-    fun addCar(car: TrainCar) {
-        cars.add(car)
-    }
+    fun addCar(car: TrainCar) = cars.add(car)
 
     fun sendTrain(passengerCount: Int) {
         println("Поезд ${route.getRoute()}, состоящий из ${cars.size} вагонов с $passengerCount пассажирами, отправлен.")
-
         var remainingPassengers = passengerCount
-
         for ((index, car) in cars.withIndex()) {
             val passengerCountInCar = if (remainingPassengers > 0) {
                 val passengersToAdd = minOf(car.capacity, remainingPassengers)
                 remainingPassengers -= passengersToAdd
                 passengersToAdd
-            } else {
-                0
-            }
+            } else 0
             println("Вагон ${index + 1}: Вместимость - ${car.capacity}, Пассажиры - $passengerCountInCar")
         }
     }
@@ -37,27 +29,25 @@ fun main() {
         "Москва", "Красноярск", "Тюмень", "Чита", "Хабаровск",
         "Владивосток", "Казань", "Самара", "Екатеринбург", "Пермь"
     )
-
     var passengerCount = 0
     var train: Train? = null
     var currentRoute: TrainRoute? = null
     while (true) {
-        println("Выберите действие:")
-        println("1 - Создать направление")
-        println("2 - Продать билеты")
-        println("3 - Сформировать поезд")
-        println("4 - Отправить поезд")
-        println("EXIT - Завершить работу")
-        val userInput = readln().uppercase()
-        when (userInput) {
+        when (getInput("""
+            Выберите действие:
+            1 - Создать направление
+            2 - Продать билеты
+            3 - Сформировать поезд
+            4 - Отправить поезд
+            EXIT - Завершить работу
+            
+        """.trimIndent()).uppercase()) {
             "1" -> {
                 val startCity = cities.random()
                 var endCity = cities.random()
-
                 while (endCity == startCity) {
                     endCity = cities.random()
                 }
-
                 currentRoute = TrainRoute(startCity, endCity)
                 println("Направление создано: ${currentRoute.getRoute()}")
                 train = Train(currentRoute)
@@ -73,19 +63,16 @@ fun main() {
                 }
                 train.cars.clear()
                 var passengersLeft = passengerCount
-
                 while (passengersLeft > 0) {
                     val carCapacity = (5..25).random()
                     val car = TrainCar(carCapacity)
                     train.addCar(car)
                     passengersLeft -= carCapacity
                 }
-
                 println("Поезд сформирован: ${currentRoute!!.getRoute()}")
                 println("Количество вагонов: ${train.cars.size}")
-                for (index in train.cars.indices) {
+                for (index in train.cars.indices)
                     println("Вместимость вагона ${index + 1}: ${train.cars[index].capacity}")
-                }
             }
             "4" -> train?.sendTrain(passengerCount)
             "EXIT" -> {
@@ -95,4 +82,9 @@ fun main() {
             else -> println("Некорректный ввод. Попробуйте еще раз.")
         }
     }
+}
+
+fun getInput(prompt: String): String {
+    print(prompt)
+    return readln()
 }
